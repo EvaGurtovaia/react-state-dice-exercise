8232bc5cfba84f75086538456ff692393e801b0c
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-11-14 14:02:10 -0600

    Adding RollDice component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import Die from "./Die";
+import RollDice from "./RollDice";
 
 function App() {
     return (
         <div className="App">
-            <Die face="five" />
-            <Die face="six" />
-            <Die face="one" />
-            <Die face="two" />
+           <RollDice />
         </div>
     );
 }
