5f9da835805ac004ee95f19801380fefe90204d4
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-11-14 14:44:42 -0600

    Styling RollDice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./App.css";
 import RollDice from "./RollDice";
 
 function App() {
     return (
         <div className="App">
-           <RollDice />
+            <RollDice />
         </div>
     );
 }
